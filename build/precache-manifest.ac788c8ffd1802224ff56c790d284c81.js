5be444a47b46f9b7120a2e1438e77ef1eec6a476
Dmitriy A <dmitriy@iinet.net.au>
2020-03-31 01:54:46 +1100

    initial load

---
+++ build/precache-manifest.ac788c8ffd1802224ff56c790d284c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eee6a3b4547049022632851f4d2be01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49c6dbe07dba31228b6",
+    "url": "/static/css/main.09143e69.chunk.css"
+  },
+  {
+    "revision": "c8b89c1e8b6108b63e48",
+    "url": "/static/js/2.e65092d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e65092d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49c6dbe07dba31228b6",
+    "url": "/static/js/main.899ee70b.chunk.js"
+  },
+  {
+    "revision": "3ecf410d7eb275d95835",
+    "url": "/static/js/runtime-main.a90ecbee.js"
+  }
+]);
